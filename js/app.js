39542ce8f4cc1334df50bad1661074fce04bc2b0
christopher33-coder <christopherpacay@gmail.com>
2021-09-29 03:49:27 -0300

    Web Finalizada

--- js/app.js
+++ js/app.js
@@ -15,4 +15,16 @@ const seccionesPagina = new fullpage("#fullpage", {
   anchors: ["inicio", "productos", "contacto"], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
   navigationTooltips: ["Inicio", "Productos", "Contacto"], // Tooltips que mostrara por cada boton.
   showActiveTooltip: false, // Mostrar tooltip activa.
+  // ──────────────────────────────────────────────────
+  //   :::::: Secciones
+  // ──────────────────────────────────────────────────
+  sectionsColor: ["#000", "#c2c2c2", "#000"], // Color de fondo de cada seccion.
+  verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+  controlArrows: true, // Flechas del slide
+  slidesNavigation: false, // Indicadores del slide
+  afterLoad: function (origin, destination) {
+    if (destination.anchor == "contacto") {
+      document.querySelector(".footer").querySelector("h2").style.opacity = 1;
+    }
+  },
 });
